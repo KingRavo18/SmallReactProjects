abf037a291ce8a6df9088db7e39b24c71506ec0c
KingRavo18 <rkingovskis14@gmail.com>
2025-08-04 13:03:48 +0300

    RNG

--- src/components/randomNumberGen.tsx
+++ src/components/randomNumberGen.tsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
+
 export default function RandomNumberGenerator() {
+
+    const [minValue, setMinValue] = useState<number>(0);
+    const [maxValue, setMaxValue] = useState<number>(0);
+    const [randomNumber, setRandomNumber] = useState<number>(0);
+    const [warning, setWarning] = useState<string>("");
+
+    const getMinValue = (event: React.ChangeEvent<HTMLInputElement>) => setMinValue(Number(event.target.value));
+    const getMaxValue = (event: React.ChangeEvent<HTMLInputElement>) => setMaxValue(Number(event.target.value));   
+
+    const generateRN = () => {
+        if(minValue < maxValue){
+            setRandomNumber(Math.round(Math.random() * (maxValue - minValue)) + minValue);
+            setWarning("");
+        }else{
+            setWarning("Minimum value cannot exceed the maximum!");
+            setMinValue(0);
+            setMaxValue(0);
+        }
+    }
+
     return(
-        <h1>Random Number Generator</h1>
+        <div className="rng-container">
+            <h1>Random Number Generator</h1>
+            <h2>{randomNumber}</h2>
+            <label>Minimum:</label>
+            <input type="number" min="0" onChange={getMinValue} value={minValue}/>
+            <label>Maxiumum:</label>
+            <input type="number" min="0" onChange={getMaxValue} value={maxValue}/>
+            <p className="warning-message">{warning}</p>
+            <button onClick={generateRN}>Generate</button>
+        </div>
     );
 }
+//Add a reset button.
+
+//Allow choosing decimal or whole number mode.
+
+//Store a history of generated numbers.
