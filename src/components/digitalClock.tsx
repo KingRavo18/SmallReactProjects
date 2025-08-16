7e79941a736282e1af1f8d8b1cec43ae70f1f538
KingRavo18 <rkingovskis14@gmail.com>
2025-08-16 13:19:13 +0300

    j

--- src/components/digitalClock.tsx
+++ src/components/digitalClock.tsx
@@ -2,28 +2,28 @@ import { useState, useEffect } from "react";
 
 export default function DigitalClock() {
 
-    const[time, setTime] = useState<Date>(new Date());
+    const [time, setTime] = useState<Date>(new Date());
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setTime(new Date())
-        }, 1000) 
+            setTime(new Date());
+        }, 1000)
 
         return () => {
             clearInterval(interval);
         }
-    }, [])
+    }, []);
 
-    const padZero = (num : number): string => (num < 10 ? "0" : "") + num;
+    const padWithZero = (num: number) => (num < 10 ? "0" : "") + num;
 
     const handleTime = () => {
         const hours = time.getHours();
         const minutes = time.getMinutes();
         const seconds = time.getSeconds();
 
-        return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`;
+        return `${padWithZero(hours)}:${padWithZero(minutes)}:${padWithZero(seconds)}`;
     }
-
+    
     return(
         <div className="digital-clock-container">
             <h1>{handleTime()}</h1>
